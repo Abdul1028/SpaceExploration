92f135a83054407bfe2f776cc6b8dd8120782854
luciferisdev <arasool5635@gmail.com>
2024-08-31 03:39:04 +0530

    integrate with home page to display model's data

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,3 @@ class RootApp extends StatelessWidget {
     );
   }
 }
-
-Future<void> fetchUpcomingLaunches() async {
-  final Dio _dio = Dio();
-
-  try {
-    final response = await _dio
-        .get('https://ll.thespacedevs.com/2.2.0/launch/upcoming/?format=json');
-
-    if (response.statusCode == 200) {
-      final data = response.data;
-
-      // Loop through and print the required details
-      for (var launch in data['results']) {
-        print('Launch Name: ${launch['name']}');
-        print('Launch Time: ${launch['window_start']}');
-        print('Launch Status: ${launch['status']['name']}');
-        print('Rocket Name: ${launch['rocket']['configuration']['name']}');
-        print('Mission Name: ${launch['mission']['name']}');
-        print(
-            'Agency Name: ${launch['rocket']['spacecraft_stage']['spacecraft']['spacecraft_config']['manufacturer']['name']}');
-        print('-----------------------------------');
-      }
-    } else {
-      print('Failed to load launches: ${response.statusCode}');
-    }
-  } catch (e) {
-    print('Error occurred: $e');
-  }
-}
